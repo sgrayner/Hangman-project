de1eb2678d2b19f1fb08049b1b0af29163f939e6
sgrayner <srayner393@gmail.com>
2023-08-21 11:08:14 +0100

    Finishing milestone_5. Complete game.

---
+++ milestone_5.py
@@ -0,0 +1,71 @@
+import string
+import random
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    while True:
+        #print(num_lives)
+        if game.num_lives == 0:
+            print('You lost!')
+            break
+        elif game.num_letters == 0:
+            print('Congratulations. You won the game!')
+            break
+        else:
+            game._ask_for_input()
+
+class Hangman:
+    '''This class is used to set up a game of hangman.
+    Attributes:
+        word_list (list): A list of words input by the user.
+        word (str): A word randomly selected from the word list input by the user.
+        word_guessed (list): A list of strings, containing underscores for each letter in the word. Each underscore is replaced when the user guesses a missing letter.
+        num_letters (int): The number of letters in the word.
+        num_lives (int): The number of lives the user has remaining. The user looses a life each time they guess an incorrect letter.
+        '''
+    def __init__(self, word_list, num_lives):
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        
+        self.list_of_guesses = []
+
+
+    def _ask_for_input(self):
+        '''This function asks the user to input a letter. It checks if the user input is valid and passes a 
+            valid input to the check_guess function.'''
+        guess = input('Guess a letter')
+        if len(guess) != 1 or guess not in string.ascii_letters:
+            print('Invalid character. Please, enter a single alphabetical letter.')
+        elif guess in self.list_of_guesses:
+            print('You already tried that letter!')
+        else:
+            self.list_of_guesses.append(guess)
+            self._check_guess(guess)
+            
+
+
+    def _check_guess(self, guess):
+        '''This function is used to let the user know if their guess is correct or incorrect 
+            and either add a correct letter to word_guessed, or to deduct a life for an incorrect letter.
+            
+            Arguments: guess'''
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! \'{guess}\' is in the word.')
+            for position in range(len(self.word)):
+                if self.word[position] == guess:
+                    self.word_guessed[position] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f'Sorry, \'{guess}\' is not in the word. You have {self.num_lives} lives left.')
+
+        
+    
+
+            
+play_game(['apple', 'banana', 'grapes', 'orange', 'strawberry'])
